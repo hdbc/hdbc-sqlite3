c270fbbd723c1235d8544795bf332cac14407910
John Goerzen <jgoerzen@complete.org>
2005-12-28 00:22:06 +0100

    Updated SpecificDB.hs for newer testsuite

--- testsrc/SpecificDB.hs
+++ testsrc/SpecificDB.hs
@@ -1,6 +1,9 @@
 module SpecificDB where
 import Database.HDBC
 import Database.HDBC.Sqlite3
+import Test.HUnit
+
 connectDB = 
     handleSqlError (connectSqlite3 "testtmp.sql3")
 
+tests = TestLabel "Sqlite3 tests" $ TestList []
