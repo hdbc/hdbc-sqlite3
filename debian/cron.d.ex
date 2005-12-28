42e9fc2be3d5f088dbb27cfc5df3b956f9b49b37
John Goerzen <jgoerzen@complete.org>
2005-12-28 02:31:28 +0100

    Removed debian/ files that we don't need

--- debian/cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the hdbc-sqlite3 package
-#
-0 4	* * *	root	hdbc-sqlite3_maintenance
--- debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(hdbc-sqlite3):needs="X11|text|vc|wm" section="Apps/see-menu-manual"\
-  title="hdbc-sqlite3" command="/usr/bin/hdbc-sqlite3"
