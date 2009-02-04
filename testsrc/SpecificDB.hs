db1bd1f4f68dddbc6354c7243dede38262b2196e
John Goerzen <jgoerzen@complete.org>
2009-02-04 11:22:47 -0600

    Updates for fractional parts

--- testsrc/SpecificDB.hs
+++ testsrc/SpecificDB.hs
@@ -7,6 +7,8 @@ connectDB =
     handleSqlError (connectSqlite3 "testtmp.sql3")
 
 dateTimeTypeOfSqlValue :: SqlValue -> String
-dateTimeTypeOfSqlValue (SqlPOSIXTime _) = "INTEGER"
+dateTimeTypeOfSqlValue (SqlPOSIXTime _) = "TEXT"
 dateTimeTypeOfSqlValue (SqlEpochTime _) = "INTEGER"
 dateTimeTypeOfSqlValue _ = "TEXT"
+
+supportsFracTime = True
